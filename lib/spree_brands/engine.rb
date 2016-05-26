1efe0dd23041e6edef22efd49773da632579b642
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2016-05-26 01:23:08 -0500

    Add translations for en, es and es-MX

--- lib/spree_brands/engine.rb
+++ lib/spree_brands/engine.rb
@@ -15,10 +15,6 @@ module SpreeBrands
       end
     end
 
-    # initializer 'spree_store_credits.register.promotion.actions', :after => 'spree.promo.register.promotions.actions' do |app|
-    #   app.config.spree.promotions.actions <<  Spree::Promotion::Actions::GiveStoreCredit
-    # end
-
     config.to_prepare &method(:activate).to_proc
     config.autoload_paths += %W(#{config.root}/lib)
   end
